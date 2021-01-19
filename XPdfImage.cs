c2d92d017233476554f0613ede6c102e303ce15f
mayur <mayur.jansari@gmail.com>
2021-01-19 18:50:12 +0530

    update extension name

--- XPdfImage.cs
+++ XPdfImage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PdfiumLight;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -33,64 +34,67 @@ namespace PdfiumLight
             int height = (int)Math.Round(page.Height * dpi / 72f);
             return page.Render(width, height);
         }
-
-        public static Image GetImage(string Pdf, string password = null, int dpi = 300, int pageNumber = 0)
-        {
-            using (PdfDocument doc = new PdfDocument(Pdf, password))
-                return GetImage(doc, dpi, pageNumber);
-        }
-
-        public static Image GetImage(Stream Pdf, string password = null, int dpi = 300, int pageNumber = 0)
-        {
-            using (PdfDocument doc = new PdfDocument(Pdf, password))
-                return GetImage(doc, dpi, pageNumber);
-        }
-
-        public static Image GetImage(byte[] Pdf, string password = null, int dpi = 300, int pageNumber = 0)
-        {
-            using (MemoryStream ms = new MemoryStream(Pdf))
-                return GetImage(ms, password, dpi, pageNumber);
-        }
-
-        public static IEnumerable<Image> GetImages(string Pdf, string password = null, int dpi = 300)
-        {
-            using (PdfDocument doc = new PdfDocument(Pdf, password))
-                return GetImages(doc, dpi);
-        }
-
-        public static IEnumerable<Image> GetImages(Stream Pdf, string password = null, int dpi = 300)
-        {
-            using (PdfDocument doc = new PdfDocument(Pdf, password))
-                return GetImages(doc, dpi);
-        }
-
-        public static IEnumerable<Image> GetImages(byte[] Pdf, string password = null, int dpi = 300)
-        {
-            using (MemoryStream ms = new MemoryStream(Pdf))
-                return GetImages(ms, password, dpi);
-        }
-
-        public static int MoveIndex(this IEnumerable<Image> list, int index, int direction)
-        {
-            if (list == null || list.Count() < index)
-                return index;
-
-            int newIndex = index + direction;
-
-            if (newIndex < 0 || newIndex >= list.Count())
-                return 0;
-
-            return newIndex;
-        }
-
-        public static int Next(this IEnumerable<Image> list, int index)
-        {
-            return MoveIndex(list, index, 1);
-        }
-
-        public static int Previous(this IEnumerable<Image> list, int index)
-        {
-            return MoveIndex(list, index, -1);
-        }
     }
 }
+public static partial class XPdfImage
+{
+    public static Image GetImage(string Pdf, string password = null, int dpi = 300, int pageNumber = 0)
+    {
+        using (PdfDocument doc = new PdfDocument(Pdf, password))
+            return PdfiumLight.XPdfImage.GetImage(doc, dpi, pageNumber);
+    }
+
+    public static Image GetImage(Stream Pdf, string password = null, int dpi = 300, int pageNumber = 0)
+    {
+        using (PdfDocument doc = new PdfDocument(Pdf, password))
+            return PdfiumLight.XPdfImage.GetImage(doc, dpi, pageNumber);
+    }
+
+    public static Image GetImage(byte[] Pdf, string password = null, int dpi = 300, int pageNumber = 0)
+    {
+        using (MemoryStream ms = new MemoryStream(Pdf))
+            return GetImage(ms, password, dpi, pageNumber);
+    }
+
+    public static IEnumerable<Image> GetImages(string Pdf, string password = null, int dpi = 300)
+    {
+        using (PdfDocument doc = new PdfDocument(Pdf, password))
+            return PdfiumLight.XPdfImage.GetImages(doc, dpi);
+    }
+
+    public static IEnumerable<Image> GetImages(Stream Pdf, string password = null, int dpi = 300)
+    {
+        using (PdfDocument doc = new PdfDocument(Pdf, password))
+            return PdfiumLight.XPdfImage.GetImages(doc, dpi);
+    }
+
+    public static IEnumerable<Image> GetImages(byte[] Pdf, string password = null, int dpi = 300)
+    {
+        using (MemoryStream ms = new MemoryStream(Pdf))
+            return GetImages(ms, password, dpi);
+    }
+
+    public static int MoveImageIndex(this IEnumerable<Image> list, int index, int direction)
+    {
+        if (list == null || list.Count() < index)
+            return index;
+
+        int newIndex = index + direction;
+
+        if (newIndex < 0 || newIndex >= list.Count())
+            return 0;
+
+        return newIndex;
+    }
+
+    public static int NextImage(this IEnumerable<Image> list, int index)
+    {
+        return MoveImageIndex(list, index, 1);
+    }
+
+    public static int PreviousImage(this IEnumerable<Image> list, int index)
+    {
+        return MoveImageIndex(list, index, -1);
+    }
+}
+
